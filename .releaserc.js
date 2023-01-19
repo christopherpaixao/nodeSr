b41ba5477de79cea43ce4064603210bb66ddb5bb
christopherpaixao <cpaixaoneto@gmail.com>
2023-01-19 14:14:02 -0300

    Teste

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,21 @@
 module.exports = {
     repositoryUrl: 'https://github.com/christopherpaixao/nodeSr',
-    branches: ['master'],
+    branches: ["master"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        ["@semantic-release/npm", {
+          "npmPublish": false,
+          "pkgRoot": "./"
+        }],
+        ["@semantic-release/git", {
+          "assets": ["CHANGELOG.md", "./package.json", "./package-lock.json", "npm-shrinkwrap.json"],
+          "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }],
+        ["@semantic-release/exec", {
+          "publishCmd": "./publish.sh"
+        }]
+    ],
+    commitizen: { "path": "cz-conventional-changelog" }
 }
